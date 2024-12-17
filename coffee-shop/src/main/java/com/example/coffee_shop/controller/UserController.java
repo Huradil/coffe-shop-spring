0195a03ec583582dfd6ce5c31dd236168dcc9790
Huradil <nuradiltolokov@gmail.com>
2024-12-18 01:13:31 +0600

    add user detail view

--- coffee-shop/src/main/java/com/example/coffee_shop/controller/UserController.java
+++ coffee-shop/src/main/java/com/example/coffee_shop/controller/UserController.java
@@ -2,10 +2,14 @@ package com.example.coffee_shop.controller;
 
 import com.example.coffee_shop.model.CoffeeShopUser;
 import com.example.coffee_shop.model.CoffeeShopUserRole;
+import com.example.coffee_shop.model.UserBonus;
+import com.example.coffee_shop.repository.UserRepository;
 import com.example.coffee_shop.repository.UserRoleRepository;
 import com.example.coffee_shop.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -21,6 +25,8 @@ public class UserController {
     private UserService userService;
     @Autowired
     private UserRoleRepository userRoleRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/register")
     public String showRegistrationForm(Model model) {
@@ -58,5 +64,48 @@ public class UserController {
     public String showLoginForm(Model model) {
         model.addAttribute("user", new CoffeeShopUser());
         return "login";
+    }
+
+    @GetMapping("/user_detail")
+    public String showUserDetails(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        CoffeeShopUser user = userRepository.findByUsername(username);
+        UserBonus bonus = user.getBonus();
+        Double bonusAmount;
+        if (bonus != null) {
+            bonusAmount = bonus.getAmount();
+        } else {
+            bonusAmount = 0.0;
+        }
+        model.addAttribute("bonusAmount", bonusAmount);
+        model.addAttribute("user", user);
+        return "user_detail";
+    }
+
+    @PostMapping("/update_user")
+    public String updateUser(@Valid @ModelAttribute CoffeeShopUser user, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            model.addAttribute("errors", result.getAllErrors());
+            return "user_detail";
+        }
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+        CoffeeShopUser currentUser = userRepository.findByUsername(username);
+        try {
+            currentUser.setFirst_name(user.getFirst_name());
+            currentUser.setLast_name(user.getLast_name());
+            currentUser.setPhone_number(user.getPhone_number());
+            currentUser.setAddress(user.getAddress());
+            currentUser.setUsername(user.getUsername());
+            userRepository.save(currentUser);
+        } catch (Exception e) {
+            model.addAttribute("errors", e.getMessage());
+            return "user_detail";
+        }
+        if (!username.equals(user.getUsername())){
+            return "redirect:/logout";
+        }
+        return "redirect:/user_detail";
     }
 }
